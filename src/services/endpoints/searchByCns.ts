a4dcf46a43ea0b9f9b0f3efced346ef27af8f2cd
Cleginaldo <cleginaldo3000@gmail.com>
2023-12-18 20:59:03 -0300

    add search by cns

---
+++ src/services/endpoints/searchByCns.ts
@@ -0,0 +1,25 @@
+import { FindPersonByCnsType } from '@/src/services//types'
+import { api } from '@/src/services/api'
+import getOnlyNumbers from '@/src/services/utils/getOnlyNumbers'
+import { setMaskCpf } from '@/src/services/utils/setMaskCpf'
+import format from 'date-fns/format'
+
+type SearchCnsByCpf = (cns: string) => Promise<FindPersonByCnsType>
+
+export const searchPersonByCns: SearchCnsByCpf = async cns => {
+  const { data: response } = await api.get<FindPersonByCnsType>(`/cns/${getOnlyNumbers(cns)}`, {
+    params: { v: 1 },
+  })
+  const formattedResponse = {
+    ...response,
+    data: {
+      created_at: format(new Date(), `dd/MM/yyyy`),
+      cadsus: {
+        ...response.data.cadsus,
+        cpf: setMaskCpf(response.data.cadsus.cpf),
+        data_nascimento: response.data.cadsus.data_nascimento || '',
+      } as FindPersonByCnsType['data']['cadsus'],
+    } as FindPersonByCnsType['data'],
+  } as FindPersonByCnsType
+  return formattedResponse
+}
