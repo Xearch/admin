e0ca43a9d6af63661f71ffa5f930a6ce20a0fc0b
Cleginaldo <cleginaldo3000@gmail.com>
2024-01-19 18:30:10 -0300

    fix: refresh token

--- src/services/endpoints/useRefreshToken.ts
+++ src/services/endpoints/useRefreshToken.ts
@@ -1,7 +1,10 @@
 /* eslint-disable consistent-return */
+import { toastError } from '@/src/components/Notification/Notifications'
 import { authChanel, localStorageKeys } from '@/src/contexts/AuthContext'
 import { api } from '@/src/services/api'
 import useSWR, { SWRResponse } from 'swr'
+
+import { verifyToken } from '../utils/verifyToken'
 
 type Output = {
   accessToken: string
@@ -13,6 +16,7 @@ export const refreshToken: RefreshToken = async () => {
   const tokenInStorage = localStorage.getItem(localStorageKeys.token)
   if (tokenInStorage) {
     try {
+      verifyToken(tokenInStorage)
       const { data } = await api.post(`/refresh-token`)
       const { accessToken } = data
       localStorage.removeItem(localStorageKeys.token)
@@ -20,6 +24,7 @@ export const refreshToken: RefreshToken = async () => {
       api.defaults.headers.authorization = `Bearer ${accessToken}`
       return data
     } catch (error) {
+      toastError(error)
       localStorage.removeItem(localStorageKeys.token)
       localStorage.removeItem(localStorageKeys.user)
       authChanel.postMessage('signOut')
@@ -30,9 +35,9 @@ export const refreshToken: RefreshToken = async () => {
 
 export function useRefreshToken(): SWRResponse<Output, any, any> {
   return useSWR(['RefreshToken'], () => refreshToken(), {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
+    revalidateIfStale: true,
+    revalidateOnFocus: true,
+    revalidateOnReconnect: true,
     refreshInterval: 1000 * 60 * 50, // Refresh every 50 minutes
   })
 }
